d7d213e260519e17742bd971238742687026e3da
Dhyeykhanpara <78402892+Dhyeykhanpara@users.noreply.github.com>
2021-02-03 17:47:17 +0530

    Create Hello_world.java

---
+++ Hello_world.java
@@ -0,0 +1,9 @@
+package ABCD;
+
+public class Practical1 { 
+	public static void main (String[] args)
+	{
+		System.out.println(" Hello world ");
+		System.out.println(" Welcome to java, \n learning java programming is fun, ");
+}
+}
